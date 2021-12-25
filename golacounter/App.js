f3ec87f39082935766531bbd32717c06a1b476ca
Arjit Jain <arjitjain09@gmail.com>
2021-12-25 15:31:41 +0530

    project beginning

---
+++ golacounter/App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+
+const App = () => {
+  const [number, onChangeNumber] = React.useState(null);
+  const [round, onChangeRound] = React.useState(null);
+  return (
+    <View style={styles.container}>
+      <Text>Gola Counter</Text>
+      <Text>Enter Number of Players:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Players"
+        keyboardType="numeric"
+      />
+      <Text>Enter Number of Rounds:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeRound}
+        value={round}
+        placeholder="Rounds"
+        keyboardType="numeric"
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+  };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 5,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
