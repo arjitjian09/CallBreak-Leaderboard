b325d794779f63be4666722e87ab5d61f0fd1557
Runal Jain <arjitjain09@gmail.com>
2021-12-25 16:41:16 +0530

    screen added

--- golacounter/App.js
+++ golacounter/App.js
@@ -1,11 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Button, View, TextInput } from 'react-native';
+import { StyleSheet, Text, Button, View, TextInput, ImageBackground } from 'react-native';
 import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const image = { uri: "https://reactjs.org/logo-og.png" };
 
 
 const App = () => {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Calculation" component={CalculationScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+  };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 5,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image:{
+    alignSelf:'stretch',
+    flex:1,
+    justifyContent:'center',
+  }
+});
+
+
+
+
+
+const HomeScreen = ({ navigation }) => {
   const [number, onChangeNumber] = React.useState(null);
   const [round, onChangeRound] = React.useState(null);
   return (
@@ -28,26 +66,30 @@ const App = () => {
       />
       </ImageBackground>
       <StatusBar style="auto" />
-      <Button
-        title="Start"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
+    
+    <Button
+      title="Start"
+      onPress={() =>
+        navigation.navigate('Calculation', { names: ['yathart','aman','Runal','Dolly','Arjit'], rounds:5 })
+      }
+    
+    />
     </View>
   );
-  };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 5,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image:{
-    alignSelf:'stretch',
-    flex:1,
-    justifyContent:'center',
-  }
-});
+};
+const CalculationScreen = ({ navigation, route }) => {
+  return (
+  <View>
+  <Text>{route.params.rounds}</Text>
+  
+  <Text>{route.params.names[0]}</Text>
+  <Text>{route.params.names[1]}</Text>
+  <Text>{route.params.names[2]}</Text>
+  <Text>{route.params.names[3]}</Text>
+  <Text>{route.params.names[4]}</Text>
+  
+  </View>
+  );
+};
 
 export default App;
